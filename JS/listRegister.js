d876289c7f72c3023657d9cde98600e0b24250bc
Fernando Porto <feporto.a@gmail.com>
2022-09-04 19:15:22 -0300

    Added non-functional JS list scheme

--- JS/listRegister.js
+++ JS/listRegister.js
@@ -1,10 +1,35 @@
 function register() {
+    // Coleta valores dos campos
     const nameInput = document.querySelector('#nome').value;
+    const telInput = querySelector('#Tel').value;
+    const cepInput = querySelector('#CEP').value;
+
+    // Cria textnode com os valores dos campos
     const name = document.createTextNode(nameInput);
-    const createLi = document.createElement('li');
+    const tel = document.createTextNode(telInput);
+    const cep = document.createTextNode(cepInput);
+
+    // Coleta id da lista
     const list = document.querySelector('#lista');
 
-    createLi.appendChild(name);
-    list.appendChild(createLi);
-}
-  
+    // Define as constantes que criam o li e as divs da lista
+    const createLi = document.createElement('li');
+    const div_listContainer = document.createElement("div").classList.add('listContainer');
+    const div_namePlaceholder = document.createElement("div").classList.add('namePlaceholder');
+    const div_telPlaceholder = document.createElement("div").classList.add('telPlaceholder');
+    const div_cepPlaceholder = document.createElement("div").classList.add('cepPlaceholder');
+
+    // Executa a criação
+    if(nameInput === ''){
+        return;
+    } else {
+
+        div_namePlaceholder.appendChild(name);
+        div_telPlaceholder.appendChild(tel);
+        div_cepPlaceholder.appendChild(cep);
+
+        let div_item = list.createLi;
+        div_item.appendChild(div_namePlaceholder);
+
+    }
+}  
